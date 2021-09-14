dbb957c6a800e51a49e220d92685746ccea9ab24
Lauren Norgard <lauren.c.schumann@gmail.com>
2021-09-13 19:46:56 -0500

    homepage is working, login, signup, need to fix search page and save books page.

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,45 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+type Mutation {
+  login(email: String!, password: String!): Auth
+  addUser(username: String!, email: String!, password: String!): Auth
+  saveBook(content: bookInfo!): User
+  removeBook(bookId: ID!): User
+}
+type User {
+    _id: ID!
+    username: String!
+    bookCount: Int
+    email: String!
+    savedBooks: [Book]
+  }
+type Book {
+    bookId: String!
+    authors: [String]
+    description: String!
+    image: String
+    link: String!
+    title: String!
+  }
+
+  input bookInfo {
+    authors: [String]
+    description: String!
+    title: String!
+    bookId: String!
+    image: String
+    link: String!
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+  type Query {
+    me: User
+  }
+ 
+`;
+
+module.exports = typeDefs;
